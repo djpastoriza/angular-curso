c82deb1d20d51ec1eed6c40e0d08fcc8b92c3d6e
Diego Pastoriza <dpastoriza@cdt.com.ar>
2018-11-27 17:19:54 -0300

    Agregamos y modificamos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ProgressComponent } from "./progress/progress.component";
+import { Graficas1Component } from "./graficas1/graficas1.component";
+import { PagesComponent } from "./pages.component";
+import { SharedModule } from "../shared/shared.module";
+import { PAGES_ROUTES } from "./pages.routes";
+
+@NgModule({
+    declarations:[
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+
+})
+export class PagesModule{ }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumsComponent } from './breadcrums/breadcrums.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+@NgModule({
+  declarations:[
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumsComponent,
+    NopagefoundComponent
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumsComponent,
+    NopagefoundComponent
+  ]
+
+})
+export class SharedModule { }
