35f506913e7ee55de55f1d4c58e7a8d46f4e05cd
Diego Pastoriza <dpastoriza@cdt.com.ar>
2018-11-27 17:18:55 -0300

    Modificaciones en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,10 @@
 import { Routes, RouterModule } from "@angular/router";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { ProgressComponent } from "./pages/progress/progress.component";
+
 import { LoginComponent } from "./login/login.component";
 import { NopagefoundComponent } from "./shared/nopagefound/nopagefound.component";
-import { Graficas1Component } from "./pages/graficas1/graficas1.component";
-import { PagesComponent } from "./pages/pages.component";
 import { RegisterComponent } from "./login/register.component";
 
 const appRoutes: Routes = [
-
-    {
-        path: '' , 
-        component:PagesComponent,
-        children: [
-            {path: 'dashboard' , component:DashboardComponent},
-            {path: 'progress' , component:ProgressComponent},
-            {path: 'graficas1' , component:Graficas1Component},
-            {path: '' , redirectTo: '/dashboard', pathMatch: 'full'},
-        ]
-    },
     { path: 'login' , component:LoginComponent,},
     { path: 'register' , component:RegisterComponent},
     { path: '**' , component: NopagefoundComponent},
